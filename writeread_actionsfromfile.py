8f73889d6010314046ce815371d9a0c57f17926c
Dot0 <mitchell.rines@gmail.com>
2024-12-15 16:53:10 -0500

    Changed which file contains the read actions from file function. Might change again. Changed the structure of keyloggers actions. changed the capture window in monitorrun so the borders are not in the screenshot.

---
+++ writeread_actionsfromfile.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+def writeActionstoFile(filepath, actions, xOffset, yOffset):
+    with open(filepath, 'w') as file:
+        file.write('Start ' + str(xOffset) + ' ' + str(yOffset) + '\n')
+        for action in actions:
+            file.write(action + '\n')
+        file.write('End')
+
+
+def readActionsfromFile(filename):
+    previous = -1
+    offsetX = 1273
+    offsetY = 35
+    deltas = []
+    keystrokes = []
+    with open(filename, 'r') as file:
+        # Read each line in the file
+        for line in file:
+            if line[0] == 'S':
+                _, x, y = line.split(' ')
+                y = y.strip()
+                offsetX = int(x)
+                offsetY = int(y)
+
+            if line[0] == 'E':
+                previous = -1
+            # Print each line
+            if line[0] == 'c':
+                keypress = line.split('\\')[1]
+                timestamp, keypress = keypress.split(':')
+                locX, locY, state, key = keypress.split(' ')
+                locX = int(locX)
+                locY = int(locY)
+                key = key.strip()
+                keystrokes.append([locX, locY, state, key])
+
+
+                if previous != -1:
+                    now = datetime.strptime(timestamp, '%Y-%m-%d_%H-%M-%S-%f')
+                    timeDelta = now - previous
+                    if timeDelta.total_seconds() < 0:
+                        deltas.append(0)
+                    else:
+                        deltas.append(timeDelta.total_seconds())
+
+                previous = datetime.strptime(timestamp, '%Y-%m-%d_%H-%M-%S-%f')
+
+        file.close()
+        deltas.append(0)
+        return keystrokes, deltas
