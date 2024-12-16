69d4e0558f2747f1f7ff5de27828fafe329e1d98
Dot0 <mitchell.rines@gmail.com>
2024-12-15 20:43:24 -0500

    fixed a bug that included the factorio version as a reward

--- rewardextractor.py
+++ rewardextractor.py
@@ -4,17 +4,17 @@ from pytesseract import pytesseract
 from tqdm import tqdm
 import sys
 
-folder = os.path.dirname(os.path.abspath(__file__)) + sys.argv[1]
+folder = sys.argv[1]
 pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 rewards = []
 
-with open(os.path.dirname(os.path.abspath(__file__)) + sys.argv[2], 'w') as file:
+with open(sys.argv[2], 'w') as file:
     for image in tqdm(sorted(os.listdir(folder))):
         img = Image.open(folder + image) 
         img = img.crop((0, 0, 169, 25))
-        reward = pytesseract.image_to_string(img, config='--psm 10 -c tessedit_char_whitelist=(-.0123456789')
+        reward = pytesseract.image_to_string(img, config='--psm 10 -c tessedit_char_whitelist=-.buil0123456789')
         timestamp = image.split('.')[0]
-        if len(reward) == 13 or len(reward) == 14:
+        if (len(reward) == 13 or len(reward) == 14):
             tqdm.write(timestamp + ': ' + reward)
         else:
             reward = '-1.0000000000\n'
